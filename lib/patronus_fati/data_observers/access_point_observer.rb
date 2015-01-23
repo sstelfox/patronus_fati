7367cec23c820a35fd9e0c3950d00a4efbf5e8d0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-01-23 14:46:19 -0500

    Not reporting an AP as updated if it's just the last_seen_at field changing

--- lib/patronus_fati/data_observers/access_point_observer.rb
+++ lib/patronus_fati/data_observers/access_point_observer.rb
@@ -7,9 +7,14 @@ module PatronusFati::DataObservers
     before :save do
       break unless self.valid?
       if self.new?
-        #puts ('New access point detected: %s' % self.attributes.inspect)
+        puts ('New access point detected: %s' % self.attributes.inspect)
       else
-        puts ('Access Point updated (%s): %s' % [self.dirty_attributes.map { |a| a.first.name }, self.attributes.inspect])
+        dirty = self.dirty_attributes.map { |a| a.first.name }.map(&:to_s)
+        dirty.delete('last_seen_at')
+
+        unless dirty.empty?
+          puts ('Access Point updated (%s): %s' % [dirty.join(','), self.attributes.inspect])
+        end
       end
     end
   end
