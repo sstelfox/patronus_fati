428556702314d27b0e0c693909ee5298a3f95126
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-02-11 17:05:44 -0500

    Brought in the changed observer to the client model

--- lib/patronus_fati/data_observers/client_observer.rb
+++ lib/patronus_fati/data_observers/client_observer.rb
@@ -6,18 +6,40 @@ module PatronusFati::DataObservers
 
     before :save do
       break unless self.valid?
-      if self.new?
-        puts ('New Client: %s' % self.full_state.inspect)
-      else
+
+      @change_type = self.new? ? :new : :changed
+      if @change_type == :changed
         dirty = self.dirty_attributes.map { |a| a.first.name }.map(&:to_s)
         dirty.delete('last_seen_at')
-        next if dirty.empty?
 
-        changes = dirty.map { |attr| '%s => [Was: \'%s\', Now: \'%s\']' % [attr, original_attributes[PatronusFati::DataModels::Client.properties[attr]], dirty_attributes[PatronusFati::DataModels::Client.properties[attr]]] }
+        # If there weren't any meaningful changes, don't print out anything
+        # after we save.
+        if dirty.empty?
+          @change_type = nil
+          next
+        end
 
-        puts ('Updated Client: %s' % changes.join(', '))
-        puts ('Updated Client: %s' % self.full_state.inspect)
+        changes = dirty.map do |attr|
+          clean = original_attributes[PatronusFati::DataModels::Client.properties[attr]]
+          dirty = dirty_attributes[PatronusFati::DataModels::Client.properties[attr]]
+
+          [attr, [clean, dirty]]
+        end
+
+        @change_list = Hash[changes]
       end
+    end
+
+    after :save do
+      next unless @change_type
+
+      puts ('Client (%s): %s' % [@change_type, self.full_state.inspect])
+      if @change_list
+        puts ('-----> (%s): %s' % [@change_list.keys.join(','), @change_list.map { |k, v| '%: (%s => %s)' % [k, v[0], v[1]] }])
+      end
+
+      @change_type = nil
+      @change_list = nil
     end
   end
 end
