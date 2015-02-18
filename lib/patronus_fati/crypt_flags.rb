850f1097e9a04efaedbf643805917058c0a7c065
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-02-18 13:00:06 -0500

    Attempt at fixing the crypt_flags

--- lib/patronus_fati/crypt_flags.rb
+++ lib/patronus_fati/crypt_flags.rb
@@ -1,5 +1,3 @@
-require 'dm-core'
-
 module DataMapper
   class Property
     class CryptFlags < DataMapper::Property::Integer
@@ -9,61 +7,17 @@ module DataMapper
 
       def dump(value)
         unless value.nil?
-          flags = Array(value).map(&:to_s)
-          flags.uniq!
+          flags = Array(value).map(&:to_sym).uniq
+          valid_values = flags & PatronusFati::SSID_CRYPT_MAP.values.map(&:to_sym)
 
-          valid_values = flags & PatronusFati::SSID_CRYPT_MAP.values
-          PatronusFati::SSID_CRYPT_MAP.map { |k, v| valid_values.include?(v) ? k : 0 }.inject(&:+)
+          PatronusFati::SSID_CRYPT_MAP.map { |k, v| valid_values.include?(v.to_sym) ? k : 0 }.inject(&:+)
         end
       end
-
-      #def dump(value)
-      #  unless value.nil?
-      #    flags = Array(value).map { |flag| flag.to_sym }
-      #    flags.uniq!
-
-      #    flag = 0
-
-      #    flag_map.invert.values_at(*flags).each do |i|
-      #      next if i.nil?
-      #      flag += (1 << i)
-      #    end
-
-      #    flag
-      #  end
-      #end
-
-      #def initialize(model, name, options = {})
-      #  super
-
-      #  @flag_map = {}
-
-      #  flags = options.fetch(:flags, self.class.flags)
-      #  flags.each_with_index do |flag, i|
-      #    flag_map[i] = flag
-      #  end
-      #end
 
       def load(value)
         return [] if value.nil?
         PatronusFati::SSID_CRYPT_MAP.select { |k, v| (k & value) == k }.map { |k, v| v }
       end
-
-      #def load(value)
-      #  return [] if value.nil? || value <= 0
-
-      #  begin
-      #    matches = []
-
-      #    0.upto(flag_map.size - 1) do |i|
-      #      matches << flag_map[i] if value[i] == 1
-      #    end
-
-      #    matches.compact
-      #  rescue TypeError, Errno::EDOM
-      #    []
-      #  end
-      #end
 
       def typecast(value)
         case value
