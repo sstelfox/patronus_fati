70ea269b1ca7436118c6fe30ba9b31f1fc246f7c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2016-02-03 10:47:46 -0500

    Reorder client handling code to continue tracking status updates of access points and clients

--- lib/patronus_fati/message_processor/client.rb
+++ lib/patronus_fati/message_processor/client.rb
@@ -5,32 +5,38 @@ module PatronusFati::MessageProcessor::Client
     # We don't care about objects that would have expired already...
     return if obj[:lasttime] < PatronusFati::DataModels::Client.current_expiration_threshold
 
-    # These potentially represent wired assets leaking through the WiFi and
-    # devices not following the 802.11 spec.
-    return if %w( unknown from_ds ).include?(obj[:type]) || obj[:mac].nil?
+    unless obj[:bssid].nil? || obj[:bssid].empty? || obj[:bssid] == obj[:mac]
+      return unless (ap = PatronusFati::DataModels::AccessPoint.first(bssid: obj[:bssid]))
+      ap.seen!
+    end
 
     # Some messages from kismet come in corrupted with partial MACs. We care
     # not for them, just drop the bad data.
     return unless obj[:mac].match(/^([0-9a-f]{2}[:-]){5}[0-9a-f]{2}$/)
 
     client_info = client_data(obj.attributes)
-    client = PatronusFati::DataModels::Client.first_or_create({bssid: obj[:mac]}, client_info)
-    client.update(client_info)
+
+    # These potentially represent wired assets leaking through the WiFi and
+    # devices not following the 802.11 spec.
+    if %w( unknown from_ds ).include?(obj[:type]) || obj[:mac].nil?
+      client = PatronusFati::DataModels::Client.first({bssid: obj[:mac]})
+    else
+      client = PatronusFati::DataModels::Client.first_or_create({bssid: obj[:mac]}, client_info)
+    end
+    client.update(client_info) if client
 
     # Don't deal in associations that are outside of our connection expiration
     # time...
     return if obj[:lasttime] < PatronusFati::DataModels::Connection.current_expiration_threshold
 
     # Handle the associations
-    unless obj[:bssid].nil? || obj[:bssid].empty? || obj[:bssid] == obj[:mac]
-      return unless (ap = PatronusFati::DataModels::AccessPoint.first(bssid: obj[:bssid]))
-      ap.seen!
-
+    if ap && client
       if (conn = PatronusFati::DataModels::Connection.connected.first(client: client, access_point: ap))
         conn.seen!
       else
         average =  (obj[:datapackets] == 0 ? 0 : obj[:datasize] / obj[:datapackets])
 
+        # Create a connection only if it meets our thresholding logic below
         return unless !(obj[:gatewayip].nil? || obj[:ip].nil?) ||
           (average >= 156 && obj[:datapackets] > 10) ||
           (average >= 110 && obj[:datapackets] > 50)
