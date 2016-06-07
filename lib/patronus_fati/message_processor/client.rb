78ec3b069eb03acdbdde83fb7c949339015a3d40
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2016-06-07 16:30:12 -0400

    Track from_ds messages when we already know about the AP. If we don't then it's likely a wired asset

--- lib/patronus_fati/message_processor/client.rb
+++ lib/patronus_fati/message_processor/client.rb
@@ -5,9 +5,11 @@ module PatronusFati::MessageProcessor::Client
     # We don't care about objects that would have expired already...
     return if obj[:lasttime] < PatronusFati::DataModels::Client.current_expiration_threshold
 
+    # obj[:mac] is the client's MAC address
+    # obj[:bssid] is the AP's MAC address
     unless obj[:bssid].nil? || obj[:bssid].empty? || obj[:bssid] == obj[:mac]
-      return unless (ap = PatronusFati::DataModels::AccessPoint.first(bssid: obj[:bssid]))
-      ap.seen!
+      ap = PatronusFati::DataModels::AccessPoint.first(bssid: obj[:bssid])
+      ap.seen! if ap
     end
 
     # Some messages from kismet come in corrupted with partial MACs. We care
@@ -18,12 +20,20 @@ module PatronusFati::MessageProcessor::Client
 
     # These potentially represent wired assets leaking through the WiFi and
     # devices not following the 802.11 spec.
-    return if %w( unknown from_ds ).include?(obj[:type]) || obj[:mac].nil?
-
-    client = PatronusFati::DataModels::Client.first_or_create({bssid: obj[:mac]}, client_info)
-    return unless client
-
-    client.update(client_info)
+    if %w( unknown from_ds ).include?(obj[:type]) || obj[:mac].nil?
+      # We only care about these assets if the packet is actually coming from
+      # an access point. If it's not coming from an access point than it is
+      # most likely it is a wired client leaking through.
+      #
+      # It is possible but unlikely and unusual that we just haven't seen this
+      # AP yet. Not recording it now will just delay the 'seeing' of the client
+      # a little bit.
+      return unless ap
+      client = PatronusFati::DataModels::Client.first({bssid: obj[:mac]})
+    else
+      client = PatronusFati::DataModels::Client.first_or_create({bssid: obj[:mac]}, client_info)
+    end
+    client.update(client_info) if client
 
     # Don't deal in associations that are outside of our connection expiration
     # time...
