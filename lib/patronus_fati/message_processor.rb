ed073c0794d53a6fc1b5b0cbeb4d9ead2ddd2c07
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2017-01-25 11:51:26 -0500

    Reduce the initial blast threshold to account for kismet's poor time keeping

--- lib/patronus_fati/message_processor.rb
+++ lib/patronus_fati/message_processor.rb
@@ -73,7 +73,7 @@ module PatronusFati
     end
 
     def self.handle(message_obj)
-      if !PatronusFati.past_initial_flood? && @last_msg_received && (Time.now.to_f - @last_msg_received) >= 1.0
+      if !PatronusFati.past_initial_flood? && @last_msg_received && (Time.now.to_f - @last_msg_received) >= 0.8
         PatronusFati.past_initial_flood!
       end
       @last_msg_received = Time.now.to_f
