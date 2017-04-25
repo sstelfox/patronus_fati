9c8dcd230b77e912aa46680ac73d11ff4c5227f2
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2017-04-25 13:45:55 -0400

    Finish covering the challenging bits of client state announcement

--- spec/patronus_fati/data_models/client_spec.rb
+++ spec/patronus_fati/data_models/client_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe(PatronusFati::DataModels::Client) do
   it_behaves_like 'a common stateful model'
 
   context '#announce_changes' do
+    before(:each) do
+      PatronusFati::DataModels::AccessPoint.instance_variable_set(:@instances, nil)
+      PatronusFati::DataModels::Client.instance_variable_set(:@instances, nil)
+      PatronusFati::DataModels::Connection.instance_variable_set(:@instances, nil)
+    end
+
     it 'should emit no events when the client isn\'t valid' do
       expect(subject).to receive(:dirty?).and_return(true)
       expect(subject).to receive(:valid?).and_return(false)
@@ -41,11 +47,72 @@ RSpec.describe(PatronusFati::DataModels::Client) do
       subject.announce_changes
     end
 
-    it 'should emit a changed client event when dirty and synced as offline'
-    it 'should emit an offline client event when the client becomes inactive'
-    it 'should reset the presence first_seen value when announced offline'
-    it 'should remove itself from access points as a connected client when announced offline'
-    it 'should mark active connections with a lost link when announced offline'
+    it 'should emit a changed client event when dirty and synced as offline' do
+      subject.mark_synced
+      expect(subject.active?).to be_falsey
+
+      subject.presence.mark_visible
+
+      expect(subject).to receive(:valid?).and_return(true)
+      expect(PatronusFati.event_handler).to receive(:event).with(:client, :changed, anything, anything)
+      subject.announce_changes
+    end
+
+    it 'should emit an offline client event when the client becomes inactive' do
+      subject.presence.mark_visible
+      subject.mark_synced
+      expect(subject.active?).to be_truthy
+
+      expect(subject).to receive(:valid?).and_return(true)
+      expect(subject).to receive(:active?).and_return(false).exactly(3).times
+
+      expect(PatronusFati.event_handler).to receive(:event).with(:client, :offline, anything, anything)
+      subject.announce_changes
+    end
+
+    it 'should reset the presence first_seen value when announced offline' do
+      subject.presence.mark_visible
+      subject.mark_synced
+      expect(subject.active?).to be_truthy
+
+      expect(subject).to receive(:valid?).and_return(true)
+      expect(subject).to receive(:active?).and_return(false).exactly(3).times
+
+      expect { subject.announce_changes }
+        .to change { subject.presence.first_seen }.to(nil)
+    end
+
+    it 'should remove itself from access points as a connected client when announced offline' do
+      bssid = 'fa:eb:dc:45:23:67'
+      dbl = double(PatronusFati::DataModels::AccessPoint)
+      PatronusFati::DataModels::AccessPoint.instances[bssid] = dbl
+
+      subject.presence.mark_visible
+      subject.add_access_point(bssid)
+      subject.mark_synced
+
+      expect(dbl).to receive(:remove_client).with(subject.local_attributes[:mac])
+      expect(subject).to receive(:active?).and_return(false).exactly(3).times
+
+      subject.announce_changes
+    end
+
+    it 'should mark active connections with a lost link when announced offline' do
+      bssid = 'fa:eb:dc:45:23:67'
+      conn_key = "#{bssid}^#{subject.local_attributes[:mac]}"
+
+      dbl = double(PatronusFati::DataModels::Connection)
+      PatronusFati::DataModels::Connection.instances[conn_key] = dbl
+
+      subject.presence.mark_visible
+      subject.add_access_point(bssid)
+      subject.mark_synced
+
+      expect(dbl).to receive(:link_lost=).with(true)
+      expect(subject).to receive(:active?).and_return(false).exactly(3).times
+
+      subject.announce_changes
+    end
 
     it 'should mark itself as synced when announced' do
       expect(subject).to receive(:dirty?).and_return(true)
@@ -55,9 +122,47 @@ RSpec.describe(PatronusFati::DataModels::Client) do
       subject.announce_changes
     end
 
-    it 'should announce the full state of the client with online syncs'
-    it 'should announce a minimal state of the client with offline syncs'
-    it 'should announce the diagnostic data of the client'
+    it 'should announce the full state of the client with online syncs' do
+      subject.presence.mark_visible
+      subject.mark_synced
+
+      data_sample = { data: 'test' }
+
+      expect(subject).to receive(:dirty?).and_return(true)
+      expect(subject).to receive(:valid?).and_return(true)
+      expect(subject).to receive(:full_state).and_return(data_sample)
+
+      expect(PatronusFati.event_handler).to receive(:event).with(:client, :changed, data_sample, anything)
+      subject.announce_changes
+    end
+
+    it 'should announce a minimal state of the client with offline syncs' do
+      subject.presence.mark_visible
+      subject.mark_synced
+
+      expect(subject).to receive(:valid?).and_return(true)
+      expect(subject).to receive(:active?).and_return(false).exactly(3).times
+
+      expect(subject.presence).to receive(:visible_time).and_return(1234)
+      min_data = {
+        'bssid' => subject.local_attributes[:mac],
+        'uptime' => 1234
+      }
+
+      expect(PatronusFati.event_handler).to receive(:event).with(:client, :offline, min_data, anything)
+      subject.announce_changes
+    end
+
+    it 'should announce the diagnostic data of the client' do
+      sample_data = { content: 'diagnostic' }
+
+      expect(subject).to receive(:dirty?).and_return(true)
+      expect(subject).to receive(:valid?).and_return(true)
+      expect(subject).to receive(:diagnostic_data).and_return(sample_data)
+
+      expect(PatronusFati.event_handler).to receive(:event).with(:client, :offline, anything, sample_data)
+      subject.announce_changes
+    end
   end
 
   context '#add_access_point'
